344b0d7650d1d3616a90a8ff315a8e653813eb8a
PersonneBot <58113393+PersonneBot@users.noreply.github.com>
2020-12-06 10:51:51 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -963,10 +963,11 @@ client.on("message", message =>{
   if(message.content.indexOf(prefix + "horaire") == 0){
     if(message.content.trim().split(" ").length >= 3){
       let embed = new Discord.MessageEmbed()
-        .setAuthor(message.member.user.displayAvatarURL + message.member.user.username)
+        .setAuthor(message.member.user.username)
         .setTitle("**" + message.content.trim().split(" ")[1] + "** à **" + message.content.trim().split(" ")[2] + "**")
         .setColor("#AA2222")
       message.channel.send(embed);
+      message.delete();
     }
     else{
       message.channel.send("Veuillez préciser aux moins 2 horaires(horaire début + horaire fin)")
